9605cec07455eea43389dbbbb83d6432c4c8a209
OmarBasit1 <omar.basit@gmail.com>
2025-03-14 01:37:16 +0000

    precommit changes

--- benchmarks/latency_model.py
+++ benchmarks/latency_model.py
@@ -2,15 +2,12 @@
 import pickle
 from pathlib import Path
 
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from benchmark_batch import BenchmarkBatchParam
 from benchmark_batch_driver import yield_benchmark_power_profiling
 from sklearn.ensemble import GradientBoostingRegressor
-from sklearn.metrics import mean_absolute_error, mean_squared_error
-from sklearn.model_selection import (LearningCurveDisplay, learning_curve,
-                                     train_test_split)
+from sklearn.model_selection import train_test_split
 from tqdm import tqdm
 
 
