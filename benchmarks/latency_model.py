4f4b8822c05607e064bd9d4e49493a6a5a0cc857
JonnyKong <jonnykong1996@gmail.com>
2025-04-24 17:56:55 +0000

    Add MLP latency model, but did not outperform GDBT
    Signed-off-by: JonnyKong <jonnykong1996@gmail.com>

--- benchmarks/latency_model.py
+++ benchmarks/latency_model.py
@@ -1,18 +1,103 @@
 # SPDX-License-Identifier: Apache-2.0
+import time
 from pathlib import Path
 
 import numpy as np
 import pandas as pd
+import torch
+import torch.nn as nn
+import torch.optim as optim
 from benchmark_batch import BenchmarkBatchParam
 from benchmark_batch_driver import gen_power_profiling_args
 from latency_and_power_model_sampler import get_cdf_data
 from lightgbm import LGBMRegressor
 from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
+from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
 
 
-def main(batch_type: str):
+class MLPRegressor(nn.Module):
+
+    def __init__(self, input_dim, lr=1e-3, epochs=2500, batch_size=32):
+        super().__init__()
+        layers = []
+        last_dim = input_dim
+        for h in [64, 64]:
+            layers.append(nn.Linear(last_dim, h))
+            layers.append(nn.ReLU())
+            last_dim = h
+        layers.append(nn.Linear(last_dim, 1))
+        self.model = nn.Sequential(*layers)
+
+        self.lr = lr
+        self.epochs = epochs
+        self.batch_size = batch_size
+        self.loss_fn = nn.MSELoss()
+        self.optimizer = optim.Adam(self.parameters(), lr=self.lr)
+
+    def fit(self, X, y, log_dir: Path, test_size=0.2, random_state=42):
+        X_train, X_val, y_train, y_val = train_test_split(
+            X, y, test_size=test_size, random_state=random_state)
+        X_train_tensor = torch.tensor(X_train, dtype=torch.float32)
+        y_train_tensor = torch.tensor(y_train, dtype=torch.float32).view(-1, 1)
+        X_val_tensor = torch.tensor(X_val, dtype=torch.float32)
+        y_val_tensor = torch.tensor(y_val, dtype=torch.float32).view(-1, 1)
+
+        train_dataset = torch.utils.data.TensorDataset(X_train_tensor,
+                                                       y_train_tensor)
+        train_loader = torch.utils.data.DataLoader(train_dataset,
+                                                   batch_size=self.batch_size,
+                                                   shuffle=True)
+
+        writer = SummaryWriter(log_dir=log_dir)
+
+        for epoch in tqdm(range(self.epochs)):
+            self.train()
+            epoch_train_loss = 0
+            for xb, yb in train_loader:
+                pred = self.model(xb)
+                loss = self.loss_fn(pred, yb)
+                self.optimizer.zero_grad()
+                loss.backward()
+                self.optimizer.step()
+                epoch_train_loss += loss.item()
+
+            inf_lat_arr = []
+            self.eval()
+            with torch.no_grad():
+                start = time.time()
+                val_pred = self.model(X_val_tensor)
+                inf_lat_arr.append(time.time() - start)
+                val_loss = self.loss_fn(val_pred, y_val_tensor).item()
+            train_loss_avg = epoch_train_loss / len(train_loader)
+
+            if epoch > 20:
+                # Skip rampup region where high losses enlarge the figure ylim
+                writer.add_scalar('Loss/Train', train_loss_avg, epoch)
+                writer.add_scalar('Loss/Validation', val_loss, epoch)
+                writer.add_scalar('Latency/Inference', np.mean(inf_lat_arr),
+                                  epoch)
+
+        writer.close()
+
+    def predict(self, X):
+        self.eval()
+        with torch.no_grad():
+            X_tensor = torch.tensor(X, dtype=torch.float32)
+            return self.model(X_tensor).squeeze().numpy()
+
+    def save_model(self, path):
+        path = Path(path)
+        torch.save(self.state_dict(), path)
+
+    def load_model(self, path, input_dim):
+        self.__init__(input_dim)  # reinitialize model
+        self.load_state_dict(torch.load(path))
+
+
+def main(batch_type: str, model_type: str):
+    assert model_type in ['gdbt', 'mlp']
     assert batch_type in ['prefill-only', 'decode-only', 'hybrid']
 
     X, Y, freqs = load_data(batch_type)
@@ -21,10 +106,17 @@ def main(batch_type: str):
         train_test_split(X, Y, freqs, test_size=0.1, random_state=0)
     Y_train_log = np.log(Y_train)
 
-    # Train Gradient Boosting Regressor
-    model = LGBMRegressor()
-    model.fit(X_train, Y_train_log)
-    model.booster_.save_model(f'latency_model_{batch_type}.txt')
+    if model_type == 'gdbt':
+        model = LGBMRegressor()
+        model.fit(X_train, Y_train_log)
+        model.booster_.save_model(
+            Path('latency_model') / f'latency_model_{batch_type}.txt')
+    else:
+        model = MLPRegressor(input_dim=len(X_train[0]))
+        model.fit(X_train, Y_train_log,
+                  Path('latency_model') / f'mlp_loss_curve_{batch_type}')
+        model.save_model(
+            Path('latency_model') / f'latency_model_{batch_type}.pt')
 
     # Predict on test set
     Y_pred_log = model.predict(X_test)
@@ -62,10 +154,12 @@ def main(batch_type: str):
               f"Relative Error Std: {rel_error_std:.4f}")
     print(
         f"Overall Mean Absolute Relative Error: {np.mean(abs_rel_error):.4f}")
-    plot_pred_error_cdf(df_errors, batch_type)
+    plot_pred_error_cdf(
+        df_errors,
+        Path('latency_model') / f'loss_cdf_{batch_type}_{model_type}.pdf')
 
 
-def plot_pred_error_cdf(df_errors: pd.DataFrame, batch_type: str):
+def plot_pred_error_cdf(df_errors: pd.DataFrame, output_path: Path):
     error_names = ['Absolute Relative Error', 'Relative Error', 'Error']
     fig, axs = plt.subplots(2, len(error_names), figsize=(15, 10))
 
@@ -91,7 +185,7 @@ def plot_pred_error_cdf(df_errors: pd.DataFrame, batch_type: str):
         ax.legend()
 
     fig.tight_layout()
-    plt.savefig(f'pred_error_cdf_{batch_type}.pdf')
+    plt.savefig(output_path)
 
 
 def load_data(batch_type: str):
@@ -165,4 +259,5 @@ def get_feat(p: BenchmarkBatchParam) -> np.ndarray:
 
 if __name__ == '__main__':
     for batch_type in ['prefill-only', 'decode-only', 'hybrid']:
-        main(batch_type)
+        main(batch_type, 'mlp')
+        main(batch_type, 'gdbt')
